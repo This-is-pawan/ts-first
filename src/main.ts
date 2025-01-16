5c4a5f8db2da540b748271a20ea01fd57bf0bec8
Pawan <154042595+This-is-pawan@users.noreply.github.com>
2025-01-16 20:06:02 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -56,9 +56,6 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
 
   </div>
 `;
-//  <h2>${Product.name}</h2>
-//  <h4>${Product.rollno}</h4>
-//  <h5>${Product.percentage}</h5>
-//  <button class="btn">${Product.class}</button>
+
 
 
