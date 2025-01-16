f16dc21633ec975a7c502f47d401bc3504c3e237
pawan <pawanjalandhara@gmail.com>
2025-01-16 17:42:20 +0530

    tssecond update

--- src/experiments/01ex.ts
+++ src/experiments/01ex.ts
@@ -1,9 +1,31 @@
 // console.log('this is a code of ts');
-let text:String='This is a code of typescript';
+let text: String = "This is a code of typescript";
+// we can also overide this text e.g
+text = "https://github.com/This-is-pawan";
+// let a=text.charAt(3);
+// let a=text.replace('/This-is-pawan','htknksdjjnk');
+type datas = {
+  products: String;
+  price: number;
+  quality: number;
+};
+const data: datas = {
+  products: "products is milk",
+  price: 23,
+  quality:1,
+};
+const target = (ts:any): string => {
+
+ return ts; 
+};
+let expt=target(data)
+  export default  expt
+// console.log(data);
+
 console.log(text);
-let num1:number=4387;
-let num2:number=9843;
-console.log(num1+num2);
-let bol:boolean=num1<=num2;
-let result=!bol
+let num1: number = 4387;
+let num2: number = 9843;
+console.log(num1 + num2);
+let bol: boolean = num1 <= num2;
+let result = !bol;
 console.log(result);
