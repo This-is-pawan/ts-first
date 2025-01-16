8dcef18e237f9e3f96f8363b846d56992b64fcc1
pawan <pawanjalandhara@gmail.com>
2025-01-16 15:35:59 +0530

    01exupdate

--- src/experiments/01ex.ts
+++ src/experiments/01ex.ts
@@ -1,1 +1,9 @@
 // console.log('this is a code of ts');
+let text:String='This is a code of typescript';
+console.log(text);
+let num1:number=4387;
+let num2:number=9843;
+console.log(num1+num2);
+let bol:boolean=num1<=num2;
+let result=!bol
+console.log(result);
