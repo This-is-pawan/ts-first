e24e4e58693f81440cea800160c0ed27c4a86da1
Pawan <154042595+This-is-pawan@users.noreply.github.com>
2025-01-21 15:34:46 +0530

    Update 01ex.ts

--- src/experiments/01ex.ts
+++ src/experiments/01ex.ts
@@ -993,7 +993,31 @@ console.log(guy);
 
 
 
-// use in react components (optional)
+// use in react components (optional if you interested should be use )
+import Component from "./components/props/index";
+
+interface person{
+  name:string;
+  id:number;
+}
+const Per :person={
+name:'harry',
+id:123,
+}
+
+const App = () => {
+
+  return (
+    
+      <main>
+        <Component name={Per.name} id={Per.id} />
+      </main>
+  
+  );
+};
+
+export default App;
+
 
 
 
